0976a584de5fabc0322b607559c2645d96b5805e
Rodolfo Pereira de Andrade <rodolfop.andrade@gmail.com>
2024-06-15 20:49:14 -0300

    Consumindo e conectando a API na MainActivity
    • Preenchendo as listas com adapter no carregamento
    • Passando dados obtidos para o layout
    • Adicionado permissão de internet no Manifest

--- app/src/main/java/br/com/rodorush/ui/MainActivity.kt
+++ app/src/main/java/br/com/rodorush/ui/MainActivity.kt
@@ -1,19 +1,69 @@
 package br.com.rodorush.ui
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import br.com.rodorush.R
 import br.com.rodorush.databinding.ActivityMainBinding
+import br.com.rodorush.model.livedata.TranslatorLiveData
+import br.com.rodorush.ui.viewmodel.TranslatorViewModel
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val tvm: TranslatorViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
-        setSupportActionBar(amb.mainTb.apply { title =
-            context.getString(R.string.app_name) })
+        setSupportActionBar(amb.mainTb.apply { title = context.getString(R.string.app_name) })
+
+        var fromLanguage = ""
+        var toLanguage = ""
+
+        val languagesAdapter =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, mutableListOf<String>())
+        with(amb) {
+            languageFromActv.apply {
+                setAdapter(languagesAdapter)
+                setOnItemClickListener { _, _, _, _ -> fromLanguage = text.toString() }
+            }
+            languageToActv.apply {
+                setAdapter(languagesAdapter)
+                setOnItemClickListener { _, _, _, _ -> toLanguage = text.toString() }
+            }
+            translateBt.setOnClickListener {
+                tvm.translate(fromLanguage, toLanguage, toEt.text.toString(), "text")
+            }
+        }
+
+        TranslatorLiveData.languageListLiveData.observe(this) { languageList ->
+            val sortedLanguages =
+                languageList.data.languages.sortedBy { it.language }.map { it.language }
+            languagesAdapter.clear()
+            languagesAdapter.addAll(sortedLanguages)
+            languagesAdapter.notifyDataSetChanged()
+            languagesAdapter.getItem(0)?.also { from ->
+                amb.languageFromActv.setText(from, false)
+                fromLanguage = from
+            }
+            languagesAdapter.getItem(languagesAdapter.count - 1)?.also { to ->
+                amb.languageToActv.setText(to, false)
+                toLanguage = to
+            }
+        }
+
+        TranslatorLiveData.translationResultLiveData.observe(this) { translationResult ->
+            with(amb) {
+                translationResult.data.translations.first().translatedText.also {
+                    toEt.setText(it)
+                }
+            }
+        }
+
+        tvm.getLanguages()
     }
 }
